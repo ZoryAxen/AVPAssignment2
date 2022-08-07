7729fc266cbec7e296dcc7b0b1c083ae788ff5d8
Yi Shen <yishen890@gmail.com>
2022-08-07 19:29:20 +0800

    Changes to python

--- SectionC/SectionC.py
+++ SectionC/SectionC.py
@@ -2,12 +2,11 @@ file = open("HRMasterlistB.txt", "r")
 totalsalary = 0
 for line in file.readlines():
     record = line.split("|")
+    hiretype = record[9]
     date = record[3].split("/")
     year = int(date[2])
-    print(year)
-    if year > 1995:
+    if year > 1995 and hiretype =="FullTime":
         totalsalary += int(record[9]) * 0.85
-        print(int(record[9]) * 0.85)
     else:
         totalsalary += int(record[9])
 file.close()
